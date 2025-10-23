debd57da73e235185a28518018cbeb359193cd10
eddie315 <esiou1785@sdsu.edu>
2025-10-23 12:42:32 -0700

    update 1 update 2 (heap.h) should be done

--- heap.h
+++ heap.h
@@ -16,6 +16,7 @@ struct MinHeap {
 
     void push(int idx, int weightArr[]) {
         data[size] = idx;
+        //uses upheap function i made to re organize the tree
         upheap(size, weightArr);
         size++;
         // TODO: insert index at end of heap, restore order using upheap()
@@ -23,9 +24,20 @@ struct MinHeap {
 
 
     int pop(int weightArr[]) {
+        //use the root at first index because its already the smallest one
+        int root = data[0];
+        // move last element into root
+data[0] = data[size-1];
+        // decrease the size of the heap as we popped it out
+        size--;
+        // do downheap to fix the  sturcture
+        downheap(0, weightArr);
+
         // TODO: remove and return smallest index
         // Replace root with last element, then call downheap()
-        return -1; // placeholder
+
+        // return the popped function
+        return root;
     }
 
     void upheap(int pos, int weightArr[]) {
@@ -37,9 +49,9 @@ struct MinHeap {
             // to find left child do (pos*2)+1
             // to find right child do (pos*2)+2
             int root = (pos - 1) / 2;
-            if ( weightArr[root] > weightArr[pos] ) {
+            if ( weightArr[data[root]] > weightArr[data[pos]]) {
                 // swaps the current child when smaller than parent
-                swap(data[pos], weightArr[root]);
+                swap(data[pos], data[root]);
                 pos = root;
             }
             //if pos is less than 0 will end the loop cycle
@@ -56,19 +68,23 @@ struct MinHeap {
             //used to find the root aka parrent same code as
             int rightchild = (pos*2)+2;
             int smallest;
-            if (weightArr[data[leftchild]] > weightArr[data[rightchild]] && rightchild < size) {
+            // quick if statment to see determine what is the smallest by checking the data and if the acc size if larger
+            //than the data of the right child and if it is it sets the smallest to the right child else...
+            if ((rightchild < size) && weightArr[data[rightchild]] < weightArr[data[leftchild]]) {
                 smallest = rightchild;
             }
             else {
+                //declares the smallest child to be left since conditions arent met above
                 smallest = leftchild;
             }
             if ( weightArr[data[smallest]] < weightArr[data[pos]]) {
-                // swaps the current child when smaller than parent
+                // swaps the current parrent downward while larger than the child
                 swap(data[pos], data[smallest]);
+                // reset the child logic as it has been shifted around by using child logic
                 pos = smallest;
                 leftchild = (pos*2)+1;
             }
-            //if pos is less than 0, will end the loop cycle
+            //once child and parents have been shifted to satafiy loop it ends with break
             else {
                 break;
             }
